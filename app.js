dbdb68069a6235cef8752073a93d4e36d8c297b7
Vadim Storozhuk <vadim.storozhuk@man.com>
2019-05-13 16:19:40 -0400

    The End. End of lecture 37

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ app.use(express.static('./public'));
 todoController(app);
 
 // listen
-app.listen(3000);
-console.log('Listening to port 3000');
+app.listen(3030);
+console.log('Listening to port 3030');
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,25 +1,57 @@
 const express = require('express'); // body-parser is now inside express
+const mongoose = require('mongoose');
 
+// Connect to Mongo
+mongoose.connect("mongodb://localhost:27017/todo", { useNewUrlParser: true });
+// Create a schema
+const todoSchema = new mongoose.Schema({
+  item: String
+});
+// Model
+const Todo = mongoose.model('Todo', todoSchema);
+
+/*
+const item1 = Todo({item: "buy flowers"}).save(err => {
+  if (err) throw err;
+  console.log("item saved");
+});
+*/
+
+/*
 let data = [
   {item: 'get milk'},
   {item: 'walk dog'},
   {item: 'kick some coding'},
 ];
+*/
 
 module.exports = app => {
 
   app.get('/todo', (req, res) => {
-    res.render('todo', {todos: data});
+    Todo.find({}, (err, data) => {
+      if (err) throw err;
+      res.render('todo', {todos: data});
+    });
   });
 
   app.post('/todo', express.urlencoded({ extended: true }), (req, res) => {
-    data.push(req.body);
-    res.json(data);
+    //Get data from the view and add it to Mongo
+    const newTodo = Todo(req.body).save((err, data) => {
+      if (err) throw err;
+      res.json(data)
+    });
+    //data.push(req.body);
+    //res.json(data);
   });
 
   app.delete('/todo/:item', (req, res) => {
-    data = data.filter(el => (el.item.replace(/ /g, "-") !== req.params.item));
-    res.json(data);
+    //Delete the item from Mongo
+    Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove((err, data) => {
+      if (err) throw err;
+      res.json(data);
+    });
+    //data = data.filter(el => (el.item.replace(/ /g, "-") !== req.params.item));
+    //res.json(data);
   });
 
 };
